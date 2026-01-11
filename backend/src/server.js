dd0438f14825acaa6ea79859c0d5627018b5d058
Talari-Ashwin-Raj <ashwin@Ashwins-MacBook-Pro-4.local>
2026-01-11 17:37:14 +0530

    final commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const prisma = require('./config/prisma')
+
 require('dotenv').config()
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
